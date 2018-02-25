4797f1b9bc907e325ad6dd3d0c8dfce79cd90673
wzuschlag <wzuschlag@gmail.com>
2018-02-25 10:54:41 -0600

    build files from class examples

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Jon Doe',
+    profilePic: 'https://www.flickr.com/photos/delicategenius/4369655755/',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  },{
+    name: 'Jane Doe',
+    profilePic: 'https://pixabay.com/en/avatar-lady-woman-female-girl-1687700/',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,44 @@
+
+var friendList = require("../data/friends.js");
+
+// ROUTING
+module.exports = function(app) {
+  // API GET Requests
+  app.get("/api/friends", function(req, res) {
+    res.json(friendList);
+  });
+  // API POST Requests
+  app.post("/api/friends", function(req, res) {
+    //grabs the new friend's scores to compare with friends in friendList array
+    var newFriendScores = req.body.scores;
+    var scoresArray = [];
+    var friendCount = 0;
+    var bestMatch = 0;
+
+    //runs through all current friends in list
+    for(var i=0; i<friendList.length; i++){
+      var scoresDiff = 0;
+      //run through scores to compare friends
+      for(var j=0; j<newFriendScores.length; j++){
+        scoresDiff += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
+      }
+
+      //push results into scoresArray
+      scoresArray.push(scoresDiff);
+    }
+
+    //after all friends are compared, find best match
+    for(var i=0; i<scoresArray.length; i++){
+      if(scoresArray[i] <= scoresArray[bestMatch]){
+        bestMatch = i;
+      }
+    }
+
+    //return bestMatch data
+    var bff = friendList[bestMatch];
+    res.json(bff);
+
+    //pushes new submission into the friendsList array
+    friendList.push(req.body);
+  });
+};
