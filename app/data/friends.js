b6df6b76f30d72807d67ff7a2f34780e77dbc19f
wzuschlag <wzuschlag@gmail.com>
2018-03-01 17:12:59 -0600

    adding pic paths

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@
 var friendList = [
   {
     name: 'Jon Doe',
-    profilePic: 'https://www.flickr.com/photos/delicategenius/4369655755/',
+    profilePic: 'https://cdn.pixabay.com/photo/2017/01/03/22/14/hero-1950778_1280.jpg',
     scores: [
       "4",
       "5",
@@ -18,18 +18,33 @@ var friendList = [
     ]
   },{
     name: 'Jane Doe',
-    profilePic: 'https://pixabay.com/en/avatar-lady-woman-female-girl-1687700/',
+    profilePic: 'https://cdn.pixabay.com/photo/2016/09/22/16/38/avatar-1687700_1280.jpg',
     scores: [
+      "5",
       "4",
       "5",
-      "5",
-      "2",
+      "3",
       "4",
       "5",
       "4",
       "4",
       "3",
-      "4"
+      "5"
+    ]
+  },{
+    name: 'Batman',
+    profilePic: 'https://orig00.deviantart.net/3610/f/2011/280/6/5/batman_face_by_artmix6-d4c3grf.jpg',
+    scores: [
+    "5",
+    "5",
+    "5",
+    "2",
+    "5",
+    "3",
+    "5",
+    "1",
+    "1",
+    "4"
     ]
   }
 ]
